077d7df962a507ffbc0d5472a18ebe02013db7ee
Mahmoud Othman <mahmoud.o.le@nono.network>
2024-07-06 14:31:09 +0300

    put together some componenets

---
+++ lib/src/models/PiggyModel.dart
@@ -0,0 +1,11 @@
+class Piggymodel {
+  final String item;
+  final String amount;
+  final String sign;
+
+  const Piggymodel({
+    required this.item,
+    required this.amount,
+    required this.sign,
+  });
+}
